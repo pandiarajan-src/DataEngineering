c50c73f8cd6e856dc6bbe992b0d6742d2cf0ec6a
Pandiarajan <pandiarajans@outlook.com>
2024-07-28 03:58:09 +0000

    adding Linux and Bash 4 DE workspace

---
+++ DataEngineeringEssentials/Linux_and_Bash_4_DE/hello_world.sh
@@ -0,0 +1,18 @@
+# Shebang indicating bash interpreter
+#! bin/bash
+
+# print hello world
+echo "Hello World"
+
+# Set an environment variable
+export MY_VAR="hello world var creation"
+
+# Print the variable
+echo $MY_VAR
+
+#define an allias
+alias ll="ls -a"
+
+#use the alias
+ll
+
